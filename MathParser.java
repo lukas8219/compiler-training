25c69f9d8085111c2d5507c87aaf21e5fd8d3f10
Lucas Polesello <lucas.c4d@gmail.com>
2022-03-01 23:28:42 -0300

    added parenthesis to separate classes

--- MathParser.java
+++ MathParser.java
@@ -17,18 +17,40 @@ public class MathParser {
         this.CURRENT_TOKEN = TOKENS.next();
     }
 
-    public Integer calculate() {
+    private Integer TERMINAL_EXPRESSION() {
         var result = number();
+
+        while (CURRENT_TOKEN.type().isMultiplication() || CURRENT_TOKEN.type().isDivision()) {
+            if (CURRENT_TOKEN.type().isMultiplication()) {
+                eat(TokenLexes.MULTIPLICATION);
+                result *= number();
+            }
+
+            if (CURRENT_TOKEN.type().isDivision()) {
+                eat(TokenLexes.DIVISION);
+                result /= number();
+            }
+        }
+
+        return result;
+    }
+
+    public int calculate() {
+        return expression();
+    }
+
+    private Integer expression() {
+        int result = TERMINAL_EXPRESSION();
 
         while (CURRENT_TOKEN.type().isSubtraction() || CURRENT_TOKEN.type().isAddition()) {
             if (CURRENT_TOKEN.type().isAddition()) {
                 eat(TokenLexes.PLUS);
-                result += number();
+                result += TERMINAL_EXPRESSION();
             }
 
             if (CURRENT_TOKEN.type().isSubtraction()) {
                 eat(TokenLexes.MINUS);
-                result -= number();
+                result -= TERMINAL_EXPRESSION();
             }
         }
 
@@ -37,8 +59,16 @@ public class MathParser {
 
     private int number() {
         var token = CURRENT_TOKEN;
-        eat(TokenLexes.INTEGER);
-        return token.getIntValue();
+        if (token.type().isLeftBracket()) {
+            eat(TokenLexes.LEFT_BRACKET);
+            var result = expression();
+            eat(TokenLexes.RIGHT_BRACKET);
+            return result;
+        } else if (token.type().isSameTypeAs(TokenLexes.INTEGER)) {
+            eat(TokenLexes.INTEGER);
+            return token.getIntValue();
+        }
+        throw new RuntimeException("Invalid input for number");
     }
 
     private void eat(TokenLexes token) {
--- MathParserTest.java
+++ MathParserTest.java
@@ -2,13 +2,26 @@ import token.AssertBuilder;
 
 public class MathParserTest {
 
+    public static void assertParser(String expression, Integer value){
+        AssertBuilder.assertThat(new MathParser(new MathLexer(expression)).calculate()).isEqualTo(value);
+    }
+
     public static void main(String[] args) {
-        var parses = new MathParser(new MathLexer("10+20*30"));
         testAdditionAndSubtraction();
+        testMultiplicationAndDivision();
+        testExpressionWithParenthesis();
+    }
+
+    private static void testExpressionWithParenthesis() {
+        assertParser("1+(20*10+(20+20*10))", 421);
     }
 
     public static void testAdditionAndSubtraction(){
-        var parsers = new MathParser(new MathLexer("5+5-3"));
-        AssertBuilder.assertThat(parsers.calculate()).isEqualTo(7);
+        assertParser("3+3+3", 9);
+        assertParser("3-3-5", -5);
+    }
+
+    private static void testMultiplicationAndDivision(){
+        assertParser("3*3-3", 6);
     }
 }
